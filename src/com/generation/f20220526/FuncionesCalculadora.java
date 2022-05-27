e4392215cf4cd52affe2808e4f659562344a463e
NachoGalaz <igalazf@gmail.com>
2022-05-26 20:00:22 -0400

    26 de mayo

--- src/com/generation/f20220526/FuncionesCalculadora.java
+++ src/com/generation/f20220526/FuncionesCalculadora.java
@@ -2,48 +2,73 @@ package com.generation.f20220526;
 
 import java.util.Scanner;
 
-public class FuncionesCalculadora {}
-/**
-}
+public class FuncionesCalculadora {
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println(" (1) sumar \n (2) restar \n (3) multiplicar \n (4) dividir \n (5) resto \n (0) salir");
+
+        int num1 = 0;
+        int num2 = 0;
+
+
+        System.out.println("ingrese un numero");
+         num1 = sc.nextInt();
+        System.out.println("ingrese un segundo numero");
+        num2 = sc.nextInt();
+
+        int opcion = 0;
+
+        do {
+            System.out.println(" (0) salir \n (1) sumar \n (2) restar \n (3) multiplicar \n (4) dividir \n (5) resto");
+
+            opcion = sc.nextInt();
+
+        }while (opcion < 0 || opcion > 5 );
+
+        /*while(opcion <0 || opcion > 5){
+
+            System.out.println(" (0) salir \n (1) sumar \n (2) restar \n (3) multiplicar \n (4) dividir \n (5) resto");
+             opcion = sc.nextInt();
+        }
+        System.out.println("Opcion invalida");*/
 
         switch (opcion) {
             case 0://SALIR
                 //accion para la opcion 1
-                System.out.println("Vuelva Pronto :)");
+                System.out.println("+-+-+-+ Vuelva Pronto :) +-+-+-+");
                 break;
             case 1://sumar
                 //accion para la opcion 1
-                System.out.println("----Suma----");
+                System.out.println("+----Suma----+");
+                System.out.println(num1 + " + " + num2 + " = " + suma(num1,num2));
                 break;
             case 2://resta
                 //accion para la opcion 2
                 System.out.println("----Resta----");
+                System.out.println(num1 + " - " + num2 + " = " + resta(num1,num2));
                 break;
             case 3://multiplicacion
                 //accion para la opcion 3
                 System.out.println("----Multiplica----");
+                System.out.println(num1 + " * " + num2 + " = " + multiplicacion(num1,num2));
                 break;
             case 4://division
                 //accion para la opcion 4
                 System.out.println("----Divide----");
+                System.out.println(num1 + " / " + num2 + " = " + division(num1,num2));
                 break;
-            case 4://division
-                //accion para la opcion 4
-                System.out.println("----Resto----");
             case 5://resto
                 //accion para la opcion 5
                 System.out.println("----Resto----");
+                System.out.println(num1 + " % " + num2 + " = " + resto(num1,num2));
+                break;
 
             default:
-                System.out.println("opcion invalida, por favor ingrese un numero del 1 al 4.");
+                System.out.println("opcion invalida, por favor ingrese un numero del 0 al 5.");
                 break;
         }
-    }
     }
 
     // Funcion suma
@@ -68,17 +93,11 @@ public class FuncionesCalculadora {}
 
 
     //Funcion resto
+
     public static Double resto (double num1, double num2) {
-        if (num1 % num2 == 0) {
-            System.out.println("El resultado es par");
-        } else {
-            System.out.println("El resultado es impar");
-        }
-        return (num1 % num2);
-    }
 
-
+        return (num1 % num2);}
 
 
 }
-**/
+
